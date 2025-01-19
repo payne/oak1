0c22091b9ba2e0da24610d72c44271f2ced03539
Matt Payne <Payne@MattPayne.org>
2025-01-18 20:04:49 -0600

    wip

---
+++ angular_todo/src/app/todo.interface.ts
@@ -0,0 +1,9 @@
+// todo.interface.ts
+export interface Todo {
+    id?: number;
+    title: string;
+    completed: boolean;
+  }
+
+  
+  
---
+++ angular_todo/src/app/todo.service.ts
@@ -0,0 +1,34 @@
+
+
+// todo.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from './todo.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  private apiUrl = '/api/todos';
+
+  constructor(private http: HttpClient) { }
+
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.apiUrl);
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.apiUrl, todo);
+  }
+
+  updateTodo(todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`${this.apiUrl}/${todo.id}`, todo);
+  }
+
+  deleteTodo(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
+
+
