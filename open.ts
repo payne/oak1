46387b86c2797b55af10e31bf79e304cd7e86dd7
Matt Payne <Payne@MattPayne.org>
2025-02-07 09:38:28 -0600

    TIL how to open a browser to a URL from deno :-)

---
+++ open.ts
@@ -0,0 +1,8 @@
+//import open from '@rdsq/open';
+import open from "jsr:@rdsq/open";
+
+await open('https://AppFarms.org');
+
+// Based on https://jsr.io/@rdsq/open
+// and https://youtu.be/ZsDqTQs3_G0?si=0cCl2rGM61yqkkXi&t=138
+
